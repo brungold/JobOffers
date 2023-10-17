096775e195aa1e88c3deca7bd8a229f9ce71275d
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-10-17 19:54:01 +0200

    added the test case should_fetch_offers_and_save_them_all_when_repository_is_empty

--- src/test/java/com/joboffers/domain/offer/OfferFacadeTest.java
+++ src/test/java/com/joboffers/domain/offer/OfferFacadeTest.java
@@ -1,11 +1,25 @@
 package com.joboffers.domain.offer;
 
+import com.joboffers.domain.offer.dto.OfferRequestDto;
+import com.joboffers.domain.offer.dto.OfferResponseDto;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 
 class OfferFacadeTest {
     @Test
     public void should_fetch_offers_and_save_them_all_when_repository_is_empty() {
+        //when
+        OfferFacade offerFacade = new OfferFacadeTestConfiguration().offerFacadeForTests();
+        assertThat(offerFacade.findAllOffers()).isEmpty();
 
+        //given
+        List<OfferResponseDto> result = offerFacade.fetchAllOffersAndSaveAllIfNotExists();
+
+        //then
+        assertThat(result).hasSize(4);
     }
 
     @Test
