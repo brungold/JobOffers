96a9f32251482bafbc36772f7dbc02a173d4b112
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-10-17 13:15:04 +0200

    created the base class for module Offer. Created the Repository interfaces Repository and Fetchable with the JobOfferResponse, the dto for OfferFetchable interface

---
+++ src/main/java/com/joboffers/domain/offer/OfferFetchable.java
@@ -0,0 +1,9 @@
+package com.joboffers.domain.offer;
+
+import com.joboffers.domain.offer.dto.JobOfferResponse;
+
+import java.util.List;
+
+public interface OfferFetchable {
+    List<JobOfferResponse> fetchAllOffers();
+}
