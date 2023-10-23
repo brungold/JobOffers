24472bbb1f99f86df44daa50cc1beb3ac75eddab
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-10-23 02:28:47 +0200

    implemented the fetchAllOffersAndSaveAllIfNotExists method in the OfferHttpScheduler class

--- src/main/java/com/joboffers/infrastructure/offer/scheduler/OfferHttpScheduler.java
+++ src/main/java/com/joboffers/infrastructure/offer/scheduler/OfferHttpScheduler.java
@@ -1,10 +1,13 @@
 package com.joboffers.infrastructure.offer.scheduler;
 
 import com.joboffers.domain.offer.OfferFacade;
+import com.joboffers.domain.offer.dto.OfferResponseDto;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @AllArgsConstructor
@@ -13,7 +16,9 @@ public class OfferHttpScheduler {
     private final OfferFacade offerFacade;
 
     @Scheduled(cron = "")
-    public void f() {
-        log.info("");
+    public List<OfferResponseDto> fetchAllOffersAndSaveAllIfNotExists() {
+        log.info("Fetching offers has started");
+        List<OfferResponseDto> offerResponseDtosList = offerFacade.fetchAllOffersAndSaveAllIfNotExists();
+        return offerResponseDtosList;
     }
 }
