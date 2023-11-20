4eadb5ebd730d6c3e1b35ac3c91707f19d7de4e8
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-11-20 13:14:21 +0100

    improved the OfferHttp class so that it connects to the Pracuj.pl website searching for offers in Warsaw for the given argument in the "junior java" search engine and correctly creates objects with job offers adapted to JobOfferResponse

--- src/main/java/com/joboffers/infrastructure/offer/http/pracujplscrapper/OfferHttpPracujPlScrapper.java
+++ src/main/java/com/joboffers/infrastructure/offer/http/pracujplscrapper/OfferHttpPracujPlScrapper.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.ResourceAccessException;
@@ -17,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.joboffers.infrastructure.offer.http.pracujplscrapper.PracujPlMapper.mapFromJobOfferNodesToListJobOfferResponse;
 
@@ -27,35 +29,42 @@ public class OfferHttpPracujPlScrapper implements OfferFetchable {
     private final String uri;
     private final int port;
 
+    // GET https://it.pracuj.pl/praca/junior%20java;kw/warszawa;wp?rd=30
+
     public List<JobOfferResponse> fetchAllOffers() {
         String url = buildUrl();
         String responseBody = fetchResponseBody(url);
-        Document parsedDocument = parseHtml(responseBody);
-        String jsonText = extractJsonText(parsedDocument);
-        List<JobOfferResponse> response = parseJson(jsonText);
-        return response;
+        return parseHtml(responseBody);
     }
 
-    // GET https://it.pracuj.pl/praca/junior%20java;kw/warszawa;wp?rd=30
-
-    private List<JobOfferResponse> parseJson(String jsonText) {
+    private List<JobOfferResponse> parseHtml(String html) {
         log.info("Started fetching offers from Pracuj.p using http client and jsoup");
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            JsonNode jsonNode = objectMapper.readTree(jsonText);
-            JsonNode jobOffersNode = jsonNode.at("/props/pageProps/data/jobOffers/groupedOffers");
+        Document document = Jsoup.parse(html);
 
+        Elements jobOfferElements = document.select("div.c13gi8t");
 
-            List<JobOfferResponse> mappedJobOfferResponses = mapFromJobOfferNodesToListJobOfferResponse(jobOffersNode);
-            log.info("Success Response Body Returned: " + mappedJobOfferResponses);
-            return PracujPlService.filterBySeniorityTechnologyAndPlaceInUrl(mappedJobOfferResponses);
+        List<JobOfferResponse> jobOffers = jobOfferElements.stream()
+                .map(offerElement -> {
+                    Element titleElement = offerElement.selectFirst("h2[data-test='offer-title'] a");
+                    String jobTitle = (titleElement != null) ? titleElement.text() : "Unknown Title";
 
-        } catch (ResourceAccessException e) {
-            log.error("Error while fetching offers from Pracuj.pl using http client and jsoup: " + e.getMessage());
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
-        } catch (JsonProcessingException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error processing JSON: " + e.getMessage());
-        }
+                    Element companyElement = offerElement.selectFirst("h4[data-test='text-company-name']");
+                    String companyName = (companyElement != null) ? companyElement.text() : "Unknown Company";
+
+                    // Zmiany w porównaniu do poprzedniego kodu
+                    Element linkElement = titleElement != null ? titleElement : offerElement.selectFirst("a[data-test='link-offer']");
+                    String offerUrl = (linkElement != null) ? linkElement.attr("href") : "";
+
+                    // Sprawdzenie czy istnieje element salary
+                    Element salaryElement = offerElement.selectFirst("span[data-test='offer-salary']");
+                    String salary = (salaryElement != null) ? salaryElement.text() : "Not Available";
+
+                    return new JobOfferResponse(jobTitle, companyName, salary, offerUrl);
+                })
+                .collect(Collectors.toList());
+
+        log.info("Success Response Body Returned: " + jobOffers);
+        return PracujPlService.filterBySeniorityTechnologyAndPlaceInUrl(jobOffers);
     }
 
     private String fetchResponseBody(String url) {
@@ -64,20 +73,8 @@ public class OfferHttpPracujPlScrapper implements OfferFetchable {
     }
 
     private String buildUrl() {
-//        String baseUrl = "https://it.pracuj.pl/praca/";
         String searchQuery = "junior%20java;kw";
         String location = "warszawa;wp?rd=30";
         return String.format("%s%s/%s", uri, searchQuery, location);
-    }
-
-
-
-    private Document parseHtml(String html) {
-        return Jsoup.parse(html);
-    }
-
-    private String extractJsonText(Document document) {
-        Element scriptElement = document.selectFirst("script#__NEXT_DATA__");
-        return scriptElement.html();
     }
 }
